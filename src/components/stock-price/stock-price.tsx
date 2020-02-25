607b3b7d3bb9e8097b95e9c9508c2419597129a0
brunobc <brunobezerrachaves@gmail.com>
2020-02-25 18:58:00 -0300

    Two way binding and input validation

--- src/components/stock-price/stock-price.tsx
+++ src/components/stock-price/stock-price.tsx
@@ -9,34 +9,51 @@ import { AV_API_KEY } from '../../global/global';
 export class StockPrice {
   stockInpput: HTMLInputElement; 
   @Element() el: HTMLElement;
-
+  
   @State() fetchedPrice: number;
+  @State() stockUserInput: string;
+  @State() stockInputValid = false;
 
+  onUserInput(event: Event) {
+    this.stockUserInput = (event.target as HTMLInputElement).value;
+    if (this.stockUserInput.trim() !== '') {
+      this.stockInputValid = true;
+    } else {
+      this.stockInputValid = false;
+    }
+  }
+  
   onFetchStockPrice(event: Event) {
     event.preventDefault();
     // const stockSymbol = (this.el.shadowRoot.querySelector('#stock-symbol') as HTMLInputElement).value;
     const stockSymbol = this.stockInpput.value;
     fetch(
       `https://www.alphavantage.co/query?function=GLOBAL_QUOTE&symbol=${stockSymbol}&apikey=${AV_API_KEY}`
-    )
-    .then(res => res.json())
-    .then(parsedRes => {
-      this.fetchedPrice = +parsedRes['Global Quote']['05. price'];
-    })
-    .catch(err => {
-      console.log(err);
-    });
+      )
+      .then(res => res.json())
+      .then(parsedRes => {
+        this.fetchedPrice = +parsedRes['Global Quote']['05. price'];
+      })
+      .catch(err => {
+        console.log(err);
+      });
   }
-
+    
   render() {
     return [
       <form onSubmit={this.onFetchStockPrice.bind(this)}>
-        <input id="stock-symbol" ref={el => this.stockInpput = el} />
-        <button type="submit">Fetch</button>
+      <input
+        id="stock-symbol"
+        ref={el => this.stockInpput = el}
+        value={this.stockUserInput}
+        onInput={this.onUserInput.bind(this)}
+      />
+      <button type="submit" disabled={!this.stockInputValid}>Fetch</button>
       </form>,
       <div>
-        <p>Price: ${this.fetchedPrice}</p>
+      <p>Price: ${this.fetchedPrice}</p>
       </div>
     ]
   }
 }
+  
